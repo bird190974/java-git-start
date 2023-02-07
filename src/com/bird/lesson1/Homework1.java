3e8b06a280f7f5d838ff400cda2b2dd297cd17fa
SIARHEI LEBEDZEU <data7479@mail.ru>
2023-02-07 10:50:28 +0300

    add lesson1

---
+++ src/com/bird/lesson1/Homework1.java
@@ -0,0 +1,11 @@
+package com.bird.lesson1;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        task(1);
+    }
+
+    private static void task() {
+        System.out.println("first task");
+    }
+}
